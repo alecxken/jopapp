110d3a8ee0a2441a63e38b326dcf86232e5dcecc
ALEX <AKENDAGOR@ecobank.group>
2020-05-25 22:59:24 +0300

    last revised

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ Auth::routes();
 
 //Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/home', 'AdminController@index')->name('Dashboard');
+Route::get('/dashboard', 'AdminController@index')->name('Dashboard');
 Route::get('/job', 'JobController@create');
 
 
