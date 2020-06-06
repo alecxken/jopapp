095373549d7a4a9afa180b291b41feb5a97eb776
ALEX <AKENDAGOR@ecobank.group>
2020-06-06 22:55:19 +0300

    g

--- resources/views/backapp/viewapplicant.blade.php
+++ resources/views/backapp/viewapplicant.blade.php
@@ -142,15 +142,84 @@
                                 {{ Form::label('disability', ' Ever Dismissed?') }}
                                 <dd>{{$data->is_dismissed}}</dd>
                             </div>
-
-                             <div class="form-group col-sm-12">
-                               <LABEL class="label label-success">Education Details</LABEL>
+                            <div class="col-sm-12">
+                              <hr>
                             </div>
 
-                              <div class="form-group col-sm-12">
-                               {{ Form::label('disability', ' Education Details') }}
+                             <div class="form-group col-sm-6">
+                                <hr>
+                               <dt class="label label-success">Education Details</dt>
+                          {{--   </div>
+
+                              <div class="form-group col-sm-6">
+                               {{ Form::label('disability', ' Education Details') }} --}}
+                               
                                 @foreach(explode(',', $data->education) as $edu)
                                 <dd>{{$edu}}</dd><br>
+                                @endforeach
+                            </div>
+
+                             <div class="form-group col-sm-6">
+                              <hr>
+                               <dt class="label label-primary">Membership Details</dt>
+                            {{-- </div>
+
+                              <div class="form-group col-sm-6">
+                               {{ Form::label('disability', ' Education Details') }} --}}
+                        
+                                @foreach(explode(',', $data->membership) as $membership)
+                                <dd>{{$membership}}</dd><br>
+                                @endforeach
+                            </div>
+
+
+                           <div class="form-group col-sm-6">
+                            <hr>
+                               <dt class="label label-warning">Certificate Details</dt>
+                         {{--    </div>
+
+                              <div class="form-group col-sm-6">
+                               {{ Form::label('disability', ' Education Details') }} --}}
+                               
+                                @foreach(explode(',', $data->certificates) as $certificates)
+                                <dd>{{$certificates}}</dd><br>
+                                @endforeach
+                            </div>
+
+                              <div class="form-group col-sm-6">
+                                <hr>
+                               <dt class="label label-success">Employer Details</dt>
+                          {{--   </div>
+
+                              <div class="form-group col-sm-6"> --}}
+                        
+                                @foreach(explode(',', $data->employer) as $employer)
+                                <dd>{{$employer}}</dd><br>
+                                @endforeach
+                            </div>
+
+                              <div class="form-group col-sm-6">
+                                 <hr>
+                               <dt class="label label-info">Other Training/Skills Details</dt>
+                          {{--   </div>
+
+                              <div class="form-group col-sm-6"> --}}
+                            
+                                @foreach(explode(',', $data->other_training) as $other_training)
+                                <dd>{{$other_training}}</dd><br>
+                                @endforeach
+                            </div>
+
+                              <div class="form-group col-sm-6">
+                                     <hr>
+                               <dt class="label label-primary">Referees Details</dt>
+                          
+                           {{--  </div>
+
+                              <div class="form-group col-sm-6"> --}}
+                           {{--     {{ Form::label('disability', ' Education Details') }} --}}
+                                @foreach(explode(',', $data->referees) as $referees)
+                                <dd>{{$referees}}</dd><br>
                                 @endforeach
                             </div>
 
