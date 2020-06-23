a1284bee32f75a943696d4867cb8c3120bf80857
ALEX <AKENDAGOR@ecobank.group>
2020-06-23 12:44:52 +0300

    e

--- database/migrations/2020_06_23_071555_create_checks_table.php
+++ database/migrations/2020_06_23_071555_create_checks_table.php
@@ -13,7 +13,7 @@ class CreateChecksTable extends Migration
      */
     public function up()
     {
-        DB::statement("CREATE VIEW checks AS  SELECT ja.app_id,jb.title,concat(fname,' ',lname) as name,requirement,passed as fullfilled,j.comments FROM kura_employers k LEFT JOIN kurra_apps a ON a.token = k.token LEFT JOIN jobapps ja ON a.token = ja.token LEFT JOIN applicant_creterias j ON a.token = j.app_token LEFT JOIN jobs jb ON jb.token = j.job_token where ja.app_id <> '' Group By a.token;");
+        DB::statement("CREATE VIEW checks AS  SELECT ja.app_id,jb.title,concat(fname,' ',lname) as name,requirement,passed as fullfilled,j.comments FROM kura_employers k LEFT JOIN kurra_apps a ON a.token = k.token LEFT JOIN jobapps ja ON a.token = ja.token LEFT JOIN applicant_creterias j ON a.token = j.app_token LEFT JOIN jobs jb ON jb.token = j.job_token where ja.app_id <> '' Group By requirement;");
 
     }
 
