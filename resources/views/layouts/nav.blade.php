251687493917f2e4089c81afcad61c26a438fa40
Alex Kendagor <akendagor@ecobank.com>
2021-05-25 10:08:41 +0300

    testr

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,9 +1,9 @@
   <nav class="navbar navbar-static-top" style=" background-image: linear-gradient(to right,#1f96ff,#005dae);">
       <!-- Sidebar toggle button-->
-<!--       <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
         <span class="sr-only">Toggle navigation</span>
       </a>
- -->
+
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
           <!-- Messages: style can be found in dropdown.less-->
