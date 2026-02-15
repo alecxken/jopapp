adf7dcb2bca90e5e70c8f0a8559ee314405bc23c
Akendagor <akendagor@ecobank.com>
2026-02-15 22:30:15 +0300

    jjj

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,10 +10,11 @@ use App\ApplicantsView;
 use App\ApplicantsData;
  use App\Charts\SampleChart;
   use App\Charts\ApplicantsChart;
-  use DB;
+
   // use App\Http\Requests\StoreDeviceRequest;
 use Rap2hpoutre\FastExcel\FastExcel;
 // use App\Jobs\EodJob;
+use Illuminate\Support\Facades\DB;
 class AdminController extends Controller
 {
     /**
@@ -21,60 +22,54 @@ class AdminController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        $jobs = Job::all()->count();
-        $jobapp = Jobapp::all()->count();
-
-        $user_count = Jobapp::leftJoin('jobs', 'jobs.token', '=', 'jobapps.ref_token')->leftJoin('users', 'users.id', '=', 'jobapps.captured_by')->whereNotNull('jobs.title')
-        ->select(DB::raw("count(jobapps.id) as count"),'users.name')
-     ->groupBy('users.name')->get()->pluck('count');;
-
-     $user_name = Jobapp::leftJoin('jobs', 'jobs.token', '=', 'jobapps.ref_token')->leftJoin('users', 'users.id', '=', 'jobapps.captured_by')->whereNotNull('jobs.title')
-        ->select(DB::raw("count(jobapps.id) as count"),'users.name')
-     ->groupBy('users.name')->get()->pluck('name');
-
-       $useractivity = Jobapp::leftJoin('jobs', 'jobs.token', '=', 'jobapps.ref_token')->leftJoin('users', 'users.id', '=', 'jobapps.captured_by')->whereNotNull('jobs.title')
-        ->select(DB::raw("count(jobapps.id) as count"),'users.name','jobapps.created_at')
-     ->groupBy('jobapps.created_at')->get();
-
-     //return $useractivity;
-     // ->select('jobapps.token','name','jobs.title','app_status','captured_by','jobapps.ref_token','jobapps.created_at')
-
-     // return $data;
-
-$c = count($user_count);
-for ($i=0; $i < $c ; $i++) { 
-    # code...
-    $rand_color[] = "#".substr(md5(rand()), 0, 6);;
-}
 
 
+public function index()
+{
+    // Fast counts (no full table load)
+    $jobs   = Job::count();
+    $jobapp = Jobapp::count();
 
+    // One query for user stats
+    $userStats = Jobapp::query()
+        ->leftJoin('jobs', 'jobs.token', '=', 'jobapps.ref_token')
+        ->leftJoin('users', 'users.id', '=', 'jobapps.captured_by')
+        ->whereNotNull('jobs.title')
+        ->selectRaw('COUNT(jobapps.id) as count, users.name as name')
+        ->groupBy('users.name')
+        ->orderBy('users.name')
+        ->get();
 
-              $post = ApplicantsView::all()->pluck('title');
-              $num = ApplicantsView::all()->pluck('num');
+    $user_name  = $userStats->pluck('name');
+    $user_count = $userStats->pluck('count');
 
-
-              
-
-
-               //return $post;
-                $chart = new ApplicantsChart;
-                $chart->labels($post);
-                $chart->dataset('Number of Applicants', 'bar', $num);
-
-                 $sam = new SampleChart;
-                $sam->labels($user_name);
-                $sam->dataset('Data Capturers', 'bar', $user_count)->backgroundColor($rand_color);
-
-
-     
-                // $chart = Chart::create('pie', 'highcharts')
-
-                //  
-        return view('data.dashboard',compact('jobs','jobapp','chart','sam'));
+    // If no data, still render a colored bar (optional but solves "0 data" UX)
+    if ($userStats->isEmpty()) {
+        $user_name  = collect(['No data']);
+        $user_count = collect([0]);
     }
+
+    // Always generate colors to match label count
+    $rand_color = $user_name->map(function () {
+        return sprintf('#%06X', mt_rand(0, 0xFFFFFF));
+    })->toArray();
+
+    // Applicants chart (avoid querying twice)
+    $appViews = ApplicantsView::all(['title', 'num']);
+    $post = $appViews->pluck('title');
+    $num  = $appViews->pluck('num');
+
+    $chart = new ApplicantsChart;
+    $chart->labels($post);
+    $chart->dataset('Number of Applicants', 'bar', $num);
+
+    $sam = new SampleChart;
+    $sam->labels($user_name->toArray());
+    $sam->dataset('Data Capturers', 'bar', $user_count->toArray())
+        ->backgroundColor($rand_color);
+
+    return view('data.dashboard', compact('jobs', 'jobapp', 'chart', 'sam'));
+}
 
     /**
      * Show the form for creating a new resource.
@@ -88,12 +83,12 @@ for ($i=0; $i < $c ; $i++) {
 
       public function applicantdata()
         {
-            
+
             $path = storage_path('summary/');
             $files = \File::allfiles($path);
 
 
-           foreach ($files as  $path) 
+           foreach ($files as  $path)
              {
                $file = pathinfo($path);
 
